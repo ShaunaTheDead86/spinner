a5c03b16a98b2a69c77bc9ff13eb4f127fcd1103
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-18 06:59:00 -0500

    uploading spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,12 @@
+// again, I don't know why but the delays are much too short on my computer, so I increased them to 500 ms per iteration
+
+
+let animation = '|/-\\|';
+let delay = 500;
+
+for (const char of animation) {
+  setTimeout(() => {
+    process.stdout.write('\r' + char);
+  },delay);
+  delay += 500;
+}
